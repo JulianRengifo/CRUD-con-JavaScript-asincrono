636f42cfd9e75e2cab7bcebaedaee61c40454b54
JulianRengifo <jurenco1997@gmail.com>
2023-05-11 22:01:06 -0500

    Se realizaron cambios en la funcion listaClientes para acortar el codigo

--- service/client-service.js
+++ service/client-service.js
@@ -46,33 +46,10 @@ const table = document.querySelector("[data-table]")
 // Delete   -----   DELETE
 
 const listaClientes = () => {
-  //Es decir, esta clase, que prácticamente lo que estamos indicando es que va a ser una función asíncrona.
-  const promise = new Promise(function(resolve, reject) {
-
-      /* crear una nueva comunicación entre el front end y el back end. Esto lo vamos a lograr con una clase que ya viene nativa en el navegador
-  , que se llama XMLHttpRequest. */
-  const http = new XMLHttpRequest (); 
-  //es un método que va a recibir dos parámetros. El primero que va a recibir es el método, y el segundo la URL o en dénde es que queremos que nosotros realice la acción http.
-  http.open ("GET", "http://localhost:3000/perfil");
-
-  // http.send que se va a encargar de enviar la petición. Entonces desde nuestro navegador o desde nuestro proyecto está saliendo hacia el servidor, que se encuentra en esta URL.
-  http.send(); //Nos conecta con el servidor
-
-  //http.onload. Entonces lo que significa esto es que, una vez que cargues o que termines de recibir una respuesta, vas a ejecutar esta función. 
-  http.onload = () => {
-      /* JSON, recuerdan, está todo en mayúscula, punto parse, ¿y qué va a recibir? Nuestro http.response. Entonces lo que va a hacer es: recibes esto de aquí, 
-      ya vimos que es un texto. Ahora lo que va a hacer es dentro de la clase JSON hay un método que es parse, que justamente lo que nos va a ayudar es a transformarlo
-      con esto los datos ya se ingresan en el table.*/
-      const response = JSON.parse(http.response);
-      if(http.status >= 400){  //verificar si http.status es mayor o igual a 400. Esto lo que significa es que hay un error en nuestra petición.
-        reject(response)
-      }else{
-        resolve(response) //se ejecutó satisfactoriamente nuestra función.
-      }
-    };
+  //  Abre una conexion con esta url   ------    Esta la recibe y la transforma en formato json y luego tiene acceso a data
+  return fetch ("http://localhost:3000/perfil").then (respuesta => {
+    return respuesta.json()
   })
-
-  return promise
 };
 
 //En la parte del response, es decir la respuesta que tenemos, una vez que sale de lo que viene siendo nuestra promesa, se va a convertir en data.
