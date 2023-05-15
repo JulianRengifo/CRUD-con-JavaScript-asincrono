947030dd2a5212fd6b5f7347219e92255ae7a9b2
JulianRengifo <jurenco1997@gmail.com>
2023-05-14 20:38:26 -0500

    Modulo 03 clase 01 Creando un cliente

---
+++ controllers/registro_controller.js
@@ -0,0 +1,24 @@
+
+import { clientServices } from "../service/client-service.js";
+
+const formulario = document.querySelector("[data-form]")
+
+//Agrega un escuchador  -- recibe dos parametros -- el primer evento es el que se va a escuchar -- El segundo evento es una funcion
+// Entonces los formularios tienen un comportamiento ya definido dentro de JavaScript o dentro del navegador.
+// Para prevenir esto      recibiremos nuestro evento
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();  // Dentro del evento esta propiedad preventDefault lo que hace es que el formulario actue como ya esta definido
+
+    const nombre = document.querySelector("[data-nombre]").value // Con los data attributes vamos a obtener la informacion de los inputs
+    const email = document.querySelector("[data-email]").value
+    console.log(nombre, "-", email);
+
+    // Al ser esto una promesa se puede trabajar con then
+    // Crear cliente nombre , email
+    clientServices.crearCLiente(nombre, email).then( respuesta => {  
+        //Vamos entonces a decir window, que es un método que está disponible que es justamente toda la ventana o toda la pantalla. Window.location, que es la localización.
+        // .href. Lo que vamos a hacer aquí es decirle cuál es la ruta que nosotros queremos seguir o el archivo que queremos llamar. 
+        // Esto para redireccionar la pagina cuando se cree un registro nuevo aparezca registro completado
+        window.location.href = "/screens/registro_completado.html"
+    }).catch(error => console.log(error))  // En caso de que exista algún error.
+});
--- service/client-service.js
+++ service/client-service.js
@@ -12,8 +12,28 @@
 const listaClientes = () => fetch ("http://localhost:3000/perfil").then ((respuesta) => respuesta.json());
 
 
+// Esta función recibira los datos que se ingresan en registro_controller.js y que se encargue de enviárselos a nuestro servidor o a nuestro API.
+const crearCLiente = (nombre, email) => {
+   // Recibe un parametro que es la url
+  return fetch("http://localhost:3000/perfil", {
+    // El otro parametro que es un objeto el cual va a ser el metodo POST para crear un nuevo recurso
+    method: "POST",
+    //dentro de este mismo objeto, vamos a definirle los encabezados. Estos encabezados es solo como para tener un estándar o que el servidor sepa qué tipo de archivo es el que va a recibir. 
+    headers:{
+      "Content-Type": "application/json"
+    },
+    // En body este objeto es donde nosotros vamos a poner toda la información que nosotros queremos que se envíe a través del cuerpo de la petición. 
+    //La comunicación http trabaja con texto, lo que necesitamos ahora es transformar este objeto en texto.
+    // Para eso usamos JSON.stringify
+    body:JSON.stringify({nombre, email, id: uuid.v4()})  // vamos a mandar un id, pero este id en lugar de recibirlo, lo vamos a generar con esta librería uuid.
+    //y vamos a acceder a través de una URL que se encuentra en registar_cliente.html aparece como script.
+  });
+};
+
+
 export const clientServices = {
   listaClientes,
+  crearCLiente,
 };
 
 
