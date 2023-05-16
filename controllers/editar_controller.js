a2aca5110e8faaf87e1fd7dfe5dde660eebecc8b
JulianRengifo <jurenco1997@gmail.com>
2023-05-15 22:52:59 -0500

    Modulo 05 clase 01 Async/Await

--- controllers/editar_controller.js
+++ controllers/editar_controller.js
@@ -4,7 +4,7 @@ import { clientServices } from "../service/client-service.js";
 //obtenemos la informacion del formulario editar donde encontramos el nombre y el email
 const formulario = document.querySelector("[data-form]")
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
     // carga la url cuando le damos al boton editar
     const url = new URL(window.location);
     //Obtenemos el id del cliente seleccionado al dar en el boton editar
@@ -18,12 +18,14 @@ const obtenerInformacion = () => {
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email]");
 
+    /*  el then ya lo maneja aquí adentro el await, entonces la respuesta que obtengas, la vas a asignar a esta constante llamada perfil. 
+    Por lo tanto, yo ya puedo obtener esta sintaxis, en la cual voy a hacer una llamada asíncrona a clientServices.detalleCliente,
+     voy a esperar que se comunique con el servidor de la respuesta y la respuesta la vamos a obtener en perfil. */
+    const perfil = await clientServices.editarCliente(id)
     // Al obtener la informacion le decimos donde debe de llevarla en este caso aparecera en los campos como autocompletado
     //En este caso debemos usar perfil.nombre, perfil.email para que no haga conflicto con las const nombre y email
-    clientServices.editarCliente(id).then((perfil) => {
         nombre.value = perfil.nombre;
         email.value = perfil.email;
-    });
 };
 
 obtenerInformacion();
