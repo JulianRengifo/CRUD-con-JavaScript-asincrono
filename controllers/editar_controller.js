f4778d3fa0d5e7300d1ab4b67fec7ee4d50f8d1f
JulianRengifo <jurenco1997@gmail.com>
2023-05-15 23:38:07 -0500

    Modulo 05 clase 02 Lidiando con errores

--- controllers/editar_controller.js
+++ controllers/editar_controller.js
@@ -9,7 +9,8 @@ const obtenerInformacion = async () => {
     const url = new URL(window.location);
     //Obtenemos el id del cliente seleccionado al dar en el boton editar
     const id = url.searchParams.get("id");
-
+    
+    //Si no encuntra un id valido se procede al if
     if(id === null){
         window.location.href = "/screens/error.html";
     }
@@ -18,15 +19,28 @@ const obtenerInformacion = async () => {
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email]");
 
-    /*  el then ya lo maneja aquí adentro el await, entonces la respuesta que obtengas, la vas a asignar a esta constante llamada perfil. 
-    Por lo tanto, yo ya puedo obtener esta sintaxis, en la cual voy a hacer una llamada asíncrona a clientServices.detalleCliente,
-     voy a esperar que se comunique con el servidor de la respuesta y la respuesta la vamos a obtener en perfil. */
-    const perfil = await clientServices.editarCliente(id)
-    // Al obtener la informacion le decimos donde debe de llevarla en este caso aparecera en los campos como autocompletado
-    //En este caso debemos usar perfil.nombre, perfil.email para que no haga conflicto con las const nombre y email
+
+//try...catch va a funcionar exclusivamente dentro de una función asíncrona.
+//La declaración try...catch señala un bloque de instrucciones a intentar (try), y especifica una respuesta si se produce una excepción (catch).
+    try{
+            /*  el then ya lo maneja aquí adentro el await, entonces la respuesta que obtengas, la vas a asignar a esta constante llamada perfil. 
+        Por lo tanto, yo ya puedo obtener esta sintaxis, en la cual voy a hacer una llamada asíncrona a clientServices.detalleCliente,
+        voy a esperar que se comunique con el servidor de la respuesta y la respuesta la vamos a obtener en perfil. */
+        const perfil = await clientServices.editarCliente(id)
+
+        if(perfil.nombre && perfil.email){
+            // Al obtener la informacion le decimos donde debe de llevarla en este caso aparecera en los campos como autocompletado
+        //En este caso debemos usar perfil.nombre, perfil.email para que no haga conflicto con las const nombre y email
         nombre.value = perfil.nombre;
         email.value = perfil.email;
+        }else{
+            throw new Error();
+        }
+    }catch(error){
+        window.location.href = "/screens/error.html";
+    } 
 };
+
 
 obtenerInformacion();
 
